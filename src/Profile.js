27d345b54e107d1212961ec5d83c8e786f039db1
Edward White <ewhite91186@gmail.com>
2018-03-03 21:02:09 -0600

    clicking followers or following link renders correct container

--- src/Profile.js
+++ src/Profile.js
@@ -2,8 +2,38 @@ import React from 'react';
 // import ProfileUser from './'
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showFriends: false,
+      followers: true
+    }
+
+    this.showFollowing = this.showFollowing.bind(this);
+    this.showFollowers = this.showFollowers.bind(this);
+  }
   componentDidMount() {
     this.props.whichPage('Profile');
+  }
+
+  showFollowers() {
+    if (this.state.followers && this.state.showFriends) {
+      this.setState({ showFriends: false });
+    } else if (!this.state.showFriends) {
+      this.setState({ showFriends: true, followers: true });
+    } else if (!this.state.followers) {
+      this.setState({ followers: true });
+    }
+  }
+
+  showFollowing() {
+    if (!this.state.followers && this.state.showFriends) {
+      this.setState({ showFriends: false });
+    } else if (!this.state.showFriends) {
+      this.setState({ showFriends: true, followers: false });
+    } else if (this.state.followers) {
+      this.setState({ followers: false });
+    }
   }
 
 	render() {
@@ -15,14 +45,19 @@ class Profile extends React.Component {
           <h5>Display reputation here</h5>
           <div className="follow-container">
             <div>
-              <div>Followers</div>
-              <div className="follow-number">0</div>
+              <div className="follow-name" onClick={this.showFollowers}>Followers</div>
+              <div className="follow-number" onClick={this.showFollowers}>0</div>
             </div>
             <div>
-              <div>Following</div>
-              <div className="follow-number">0</div>
+              <div className="follow-name" onClick={this.showFollowing}>Following</div>
+              <div className="follow-number" onClick={this.showFollowing}>0</div>
             </div>
           </div>
+        </section>
+        <section className={this.state.showFriends ? "profile-friends" : "profile-friends hide"}>
+          <h2>
+          {this.state.followers ? <p>Followers</p> : <p>Following</p>}
+          </h2>
         </section>
       </div>
 		)
