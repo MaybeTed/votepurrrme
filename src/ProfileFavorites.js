40efd7d9434cc50ff91a0203b2232f0627ec9f0b
Edward White <ewhite91186@gmail.com>
2018-03-13 19:58:43 -0500

    created ProfileFavorites and ProfileComments components

---
+++ src/ProfileFavorites.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ProfileFavorites extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div className="folder">
+			  Favorites
+			</div>
+		)
+	}
+}
+
+export default ProfileFavorites;
