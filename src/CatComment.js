ea828a15470c3e5a3c338977f4be46e9e7cb7541
Edward White <ewhite91186@gmail.com>
2018-03-14 15:22:37 -0500

    added function to change the format of the date

--- src/CatComment.js
+++ src/CatComment.js
@@ -4,7 +4,30 @@ import { Link } from 'react-router-dom';
 class CatComment extends React.Component {
 	constructor(props) {
 		super(props);
-		console.log('CatComment constructor')
+		this.changeTime = this.changeTime.bind(this);
+	}
+
+	changeTime(str) {
+	  const months = {
+	    '01': 'January',
+	    '02': 'February', 
+	    '03': 'March',
+	    '04': 'April',
+	    '05': 'May',
+	    '06': 'June',
+	    '07': 'July',
+	    '08': 'August',
+	    '09': 'September',
+	    '10': 'October',
+	    '11': 'November',
+	    '12': 'December'
+	  }
+	  let time = str.split('-');
+	  console.log('time: ', time)
+	  let [ year, month, day ] = time;
+	  console.log('year: ', year)
+	  day = day.split('T')[0];
+	  return months[month] + ' ' + day + ', ' + year;
 	}
 
 	render() {
@@ -12,7 +35,7 @@ class CatComment extends React.Component {
 		const { created_at, name, message, user_id } = this.props.comment;
 		return (
 			<div className="cat-comment">
-			  <p>{created_at}</p>
+			  <p>{this.changeTime(created_at)}</p>
 			  <p className="indent"><Link to={`/profile/${user_id}`} ><span className="username">{name}</span></Link> says</p>
 			  <p>{message}</p>
 
