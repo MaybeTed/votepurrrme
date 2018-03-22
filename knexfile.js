833d20574226dc2ca7b474dfc2259e0be2f5d9e8
Edward White <ewhite91186@gmail.com>
2018-03-22 18:11:26 -0500

    changed vars for production

--- knexfile.js
+++ knexfile.js
@@ -12,8 +12,8 @@ module.exports = {
     client: 'postgresql',
     connection: {
       host: process.env.DATABASE_HOST || '127.0.0.1',
-      user: process.env.DATABASE_USER || '',
-      password: process.env.DATABASE_PASSWORD || '',
+      user: process.env.DATABASE_USER || config.username,
+      password: process.env.DATABASE_PASSWORD || config.password,
       database: process.env.DATABASE_NAME || 'votepurrrme',
     }
   },
